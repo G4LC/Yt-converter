2cfe96ec20efd9f35fbba6527e6fd3618c68ef28
G4LC <83610229+G4LC@users.noreply.github.com>
2022-08-01 12:55:54 +0100

    Add files via upload
          (WARNING)You Must Have Python 3.6 Or Higher.

--- YTdownloader.py
+++ YTdownloader.py
@@ -4,7 +4,11 @@ link = argv[1]
 yt = YouTube(link)
 
 print("Title: ", yt.title)
+print("Age Restricted: ", yt.age_restricted)
 print("View: ", yt.views)
+print("Link To Channel: ", yt.channel_url)
+print("Publisher: ", yt.author)
+print("Day Of Being Publised: ", yt.publish_date)
 yd = yt.streams.get_highest_resolution()
 
 yd.download('downloads')
